2e92cc319aef0bb95b1d8523397be9289323ad0e
joshuacirilo <jcirilo.alegria@gmail.com>
2026-02-04 16:20:41 -0600

    feat: seccion5

--- seccion5/config/lugares.js
+++ seccion5/config/lugares.js
@@ -40,3 +40,4 @@ const lugares = [
         "coordenadas": [14.6029, -90.5108]
     }
 ];
+
--- seccion5/index.js
+++ seccion5/index.js
@@ -1,3 +1,9 @@
+//referencias
+const sidebar = document.querySelector('#sidebar');
+const mensaje = document.querySelector('#mesaje');
+
+
+//mapa
 const map = L.map('mapa', {
     center: [14.607820, -90.513863],
     zoom: 7,
@@ -13,3 +19,49 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+//funciones
+
+const definirMensaje = ([lat, lng]) => {
+
+    mensaje.classList.remove('hidden');
+    mensaje.innerText =`Las coordenadas de la zona seleccionada son: 
+    latitud: ${lat}, longitud: ${lng}` ;
+}
+
+
+const limpiarItems = () => {
+    const listadoLi = document.querySelectorAll('li');
+
+    listadoLi.forEach(li => {
+        li.classList.remove('active');
+    });
+}
+
+
+const volar = (latlng) =>{
+    map.flyTo(latlng, map.getMaxZoom());
+}
+
+
+const crearItems = () => {
+    const ul = document.createElement('ul');
+    ul.classList.add('list-group');
+    sidebar.prepend(ul);
+
+    lugares.forEach(lugar => {
+        const li = document.createElement('li');
+        li.innerText = lugar.nombre;
+        li.classList.add('list-group-item');
+        ul.appendChild(li);
+        
+        li.addEventListener('click', () => {
+            limpiarItems();
+            li.classList.add('active');
+            volar(lugar.coordenadas);
+            definirMensaje(lugar.coordenadas);
+            })
+    })
+}
+
+crearItems();
